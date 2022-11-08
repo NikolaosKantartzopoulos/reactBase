1799687cf71413568d6b029973d1348029a8a1f1
Nikolaos Kantartzopoulos <nkantartzopoulos@gmail.com>
2022-11-08 17:05:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RouteSwitch from "./RouteSwitch";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./styles/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<RouteSwitch />
-	</React.StrictMode>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
