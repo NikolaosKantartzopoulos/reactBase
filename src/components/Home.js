7314537283849dceaddbc4f1f27576ab0410329f
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2022-11-08 22:56:28 +0200

    Styled components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../styles/Button";
 
 function Home() {
 	return (
 		<>
 			<div>Home</div>
 			<Link to="about">About</Link>
+			<Button>Test button</Button>
 		</>
 	);
 }
---
+++ src/styles/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+	display: inline-block;
+	border-radius: 3px;
+	padding: 0.5rem 0;
+	margin: 0.5rem 1rem;
+	width: 11rem;
+	background: red;
+	color: black;
+	border: 2px solid white;
+`;
+
+export default Button;
